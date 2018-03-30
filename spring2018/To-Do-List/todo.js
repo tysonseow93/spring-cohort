433a25b47921b9898da1472a82245526f09d33ea
Tyson <tyson.seow@gmail.com>
2018-03-30 16:24:00 -0600

    updates 3-30-18-2

--- spring2018/To-Do-List/todo.js
+++ spring2018/To-Do-List/todo.js
@@ -1,27 +1,50 @@
-function createAList() {
-    let listname = $('#createinput').val();
-     $('.main').append(
-         '<div class="list-box">'
-            +'<div class="header">'
-                + '<input type="checkbox" class="markCompleteList">'
-                + '<div class="fa fa-trash"></div>'
-                + '<div class="headleft">'
-                    + listname
-                + '</div>'
-                +'<div class="headright">'
-                    + '<button type="button" class="addItem btn btn-primary" data-toggle="modal" data-target="#addItemModal" data-whatever="@mdo">Add Items</button>'
-                +'</div>'
-            +'</div>'
-            + '<div class="list">'
-                    + '<ul class="todolist">'
-                    + '</ul>'
-            + '</div>'
-         +'</div>');
-     $('#createinput').val("");
-     $('.main').show(this);
- }
+// function createAList() {
+//     let listname = $('#createinput').val();
+//      $('.main').append(
+//          '<div class="list-box">'
+//             +'<div class="header">'
+//                 + '<input type="checkbox" class="markCompleteList">'
+//                 + '<div class="fa fa-trash"></div>'
+//                 + '<div class="headleft">'
+//                     + listname
+//                 + '</div>'
+//                 +'<div class="headright">'
+//                     + '<button type="button" class="addItem btn btn-primary" data-toggle="modal" data-target="#addItemModal" data-whatever="@mdo">Add Items</button>'
+//                 +'</div>'
+//             +'</div>'
+//             + '<div class="list">'
+//                     + '<ul class="todolist">'
+//                     + '</ul>'
+//             + '</div>'
+//          +'</div>');
+//      $('#createinput').val("");
+//      $('.main').show(this);
+//  }
+
  $(document).ready(function() {
      let $active;
+     $(document).on( 'click', '#createBtn', function(){
+         let listname = $('#createinput').val();
+         $('.main').append(
+             '<div class="list-box">'
+             +'<div class="header">'
+             + '<input type="checkbox" class="markCompleteList">'
+             + '<div class="fa fa-trash"></div>'
+             + '<div class="headleft">'
+             + listname
+             + '</div>'
+             +'<div class="headright">'
+             + '<button type="button" class="addItem btn btn-primary" data-toggle="modal" data-target="#addItemModal" data-whatever="@mdo">Add Items</button>'
+             +'</div>'
+             +'</div>'
+             + '<div class="list">'
+             + '<ul class="todolist">'
+             + '</ul>'
+             + '</div>'
+             +'</div>');
+         $('#createinput').val("");
+         $('.main').show(this);
+     });
 
      $(document).on( 'click', '.addItem', function(e){
          let $button = $(this);
@@ -45,4 +68,48 @@ function createAList() {
      $(document).on("click", ".fa-trash",function () {
          $(this).parent().remove();
      });
+     $('#addListModal').on('shown.bs.modal', function () {
+         $('#createinput').trigger('focus')
+     });
+     $('#addItemModal').on('shown.bs.modal', function () {
+         $('#createItemInput').trigger('focus')
+     });
+
+ //THIS NEEDS WORKED ON MORE
+     $( '#addListModal' ).on( 'keypress', function( e ) {
+         if( e.keyCode === 13 ) {
+             e.preventDefault();
+             let listname = $('#createinput').val();
+             $('.main').append(
+                 '<div class="list-box">'
+                 +'<div class="header">'
+                 + '<input type="checkbox" class="markCompleteList">'
+                 + '<div class="fa fa-trash"></div>'
+                 + '<div class="headleft">'
+                 + listname
+                 + '</div>'
+                 +'<div class="headright">'
+                 + '<button type="button" class="addItem btn btn-primary" data-toggle="modal" data-target="#addItemModal" data-whatever="@mdo">Add Items</button>'
+                 +'</div>'
+                 +'</div>'
+                 + '<div class="list">'
+                 + '<ul class="todolist">'
+                 + '</ul>'
+                 + '</div>'
+                 +'</div>');
+             $('#createinput').val("");
+             $('.main').show(this);
+         }
+     });
+     $( '#addItemModal' ).on( 'keypress', function( e ) {
+         if( e.keyCode === 13 ) {
+             e.preventDefault();
+             let listItem = $('#createItemInput').val();
+             $active.find('.todolist').append('<div class="todoitem">'+'<li>'+ '<input type="checkbox" class="markComplete">' + listItem + '</li>'+'<div class="fa fa-trash"></div>'+'</div>');
+             $('#createItemInput').val("");
+         }
+     });
+ //THIS NEEDS WORKED ON MORE
+
  });
+
