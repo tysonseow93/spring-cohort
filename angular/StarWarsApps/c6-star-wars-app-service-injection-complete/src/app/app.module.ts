4aa953f80222c335b9e61b53cb4ab31d0a805391
Tyson <tyson.seow@gmail.com>
2018-06-23 19:21:56 -0600

    6-23 file reorganising

--- angular/StarWarsApps/c6-star-wars-app-service-injection-complete/src/app/app.module.ts
+++ angular/StarWarsApps/c6-star-wars-app-service-injection-complete/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
